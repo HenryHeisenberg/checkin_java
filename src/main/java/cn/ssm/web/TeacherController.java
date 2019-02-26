7a691b27cb28c88b5cea0e4300b1aa5695343a24
aaron <457523400@qq.com>
2019-02-26 21:33:52 +0800

    更新开启签到功能

--- src/main/java/cn/ssm/web/TeacherController.java
+++ src/main/java/cn/ssm/web/TeacherController.java
@@ -1,7 +1,9 @@
 package cn.ssm.web;
 
 import cn.ssm.base.BaseResult;
+import cn.ssm.model.CheckinInfo;
 import cn.ssm.model.ClassInfo;
+import cn.ssm.service.CheckinInfoService;
 import cn.ssm.service.ClassInfoService;
 import cn.ssm.utils.util.StringUtil;
 import cn.ssm.utils.util.UUIDUtils;
@@ -11,12 +13,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
 
     @Autowired
     ClassInfoService classInfoService;
+
+    @Autowired
+    CheckinInfoService checkinInfoService;
 
     @PostMapping("/createClass")
     public Object createClass(ClassInfo classInfo){
@@ -26,6 +33,25 @@ public class TeacherController {
         classInfo.setId(UUIDUtils.getUUID());
         classInfoService.insert(classInfo);
         return BaseResult.success("添加课程成功");
+    }
+
+    @PostMapping("/setCheckin")
+    public Object setCheckin(CheckinInfo checkinInfo){
+        if(StringUtils.isEmpty(checkinInfo.getClassId())){
+            return BaseResult.fail("未选择课程名");
+        }
+        if (StringUtils.isEmpty(checkinInfo.getStartTime())){
+            return BaseResult.fail("开始时间不能为空！");
+        }
+        if (StringUtils.isEmpty(checkinInfo.getEndTime())){
+            return BaseResult.fail("结束时间不能为空！");
+        }
+        if(StringUtils.isEmpty(checkinInfo.getCreateTime())){
+            checkinInfo.setCreateTime(new Date());
+        }
+        checkinInfo.setId(UUIDUtils.getUUID());
+        checkinInfoService.insert(checkinInfo);
+        return BaseResult.success("设置签到成功");
     }
 
 

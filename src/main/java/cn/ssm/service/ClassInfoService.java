fdc19337324e1c4aefe0d353b706c7eaa57b3d8b
yhw <55682874@qq.com>
2019-03-01 19:05:51 +0800

    push

--- src/main/java/cn/ssm/service/ClassInfoService.java
+++ src/main/java/cn/ssm/service/ClassInfoService.java
@@ -2,9 +2,19 @@ package cn.ssm.service;
 
 
 import cn.ssm.base.impl.BaseServiceImpl;
+import cn.ssm.mapper.ClassInfoMapper;
 import cn.ssm.model.ClassInfo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClassInfoService extends BaseServiceImpl<ClassInfo> {
+    @Autowired
+    ClassInfoMapper classInfoMapper;
+    public List<ClassInfo> findUnselected(String id) {
+        return classInfoMapper.findUnselected(id);
+    }
 }
--- src/main/java/cn/ssm/web/StudentController.java
+++ src/main/java/cn/ssm/web/StudentController.java
@@ -5,10 +5,12 @@ import cn.ssm.model.CheckinInfo;
 import cn.ssm.model.ClassInfo;
 import cn.ssm.model.StudentCheckin;
 import cn.ssm.model.StudentClass;
+import cn.ssm.model.UserInfo;
 import cn.ssm.service.CheckinInfoService;
 import cn.ssm.service.ClassInfoService;
 import cn.ssm.service.StudentCheckinService;
 import cn.ssm.service.StudentClassService;
+import cn.ssm.service.UserInfoService;
 import cn.ssm.utils.util.StringUtil;
 import cn.ssm.utils.util.UUIDUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +20,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 
 @RestController
@@ -30,48 +34,69 @@ public class StudentController {
 
     @Autowired
     StudentClassService studentClassService;
-    
+
     @Autowired
     CheckinInfoService checkinInfoService;
 
     @Autowired
     StudentCheckinService studentCheckinService;
-
     
-    @GetMapping("/getClasses")
-    public Object getClasses(){
-        ClassInfo classInfo=new ClassInfo();
-        List<ClassInfo> selectAll = classInfoService.selectAll();
-        return BaseResult.success(selectAll);
+    @Autowired
+    UserInfoService userInfoService;
+
+    //查找学生信息
+    @PostMapping("/getStudent")
+    public Object getStudent(String id) {
+        UserInfo selectByKey = userInfoService.selectByKey(id);
+        return BaseResult.success(selectByKey);
     }
     
-    @GetMapping("/getClassesById")
-    public Object getClassesById(String id){
-        if(StringUtils.isEmpty(id)){
+    // 查询未加入的课程
+    @GetMapping("/getClasses")
+    public Object getClasses(String id) {
+        List<ClassInfo> findUnselected = classInfoService.findUnselected(id);
+        return BaseResult.success(findUnselected);
+    }
+
+    // 通过学生id查询已选课程
+    @PostMapping("/getClassesById")
+    public Object getClassesById(String id) {
+        if (StringUtils.isEmpty(id)) {
             return BaseResult.fail("学生ID不能为空！");
         }
-        StudentClass studentClass=new StudentClass();
+        StudentClass studentClass = new StudentClass();
         studentClass.setStudentId(id);
         List<StudentClass> select = studentClassService.select(studentClass);
-        return BaseResult.success(select);
+        if (select.isEmpty()) {
+            return BaseResult.fail("该学生未选择任何课程");
+        }
+        List<ClassInfo> classList = new ArrayList<ClassInfo>();
+        for (StudentClass studentClassInfo : select) {
+            String classId = studentClassInfo.getClassId();
+            ClassInfo classInfo = classInfoService.selectByKey(classId);
+            classList.add(classInfo);
+        }
+        return BaseResult.success(classList);
     }
 
+    // 根据课程id查询课程详细信息
     @GetMapping("/getOneClassByClassId")
     public Object getOneClassByClassId(String id) {
-        if(StringUtils.isEmpty(id)){
+        if (StringUtils.isEmpty(id)) {
             return BaseResult.fail("课程ID不能为空！");
         }
         ClassInfo classInfo = new ClassInfo();
         ClassInfo selectByKey = classInfoService.selectByKey(id);
         return BaseResult.success(selectByKey);
     }
-    
-    @GetMapping("selectCheckOrNotById")
-    public Object selectCheckOrNot(String studentId,String classId) {
-        if(StringUtils.isEmpty(studentId)){
+
+    // 查询是否缺席
+    @PostMapping("selectCheckOrNotById")
+    public Object selectCheckOrNot(String studentId, String classId) {
+        if (StringUtils.isEmpty(studentId)) {
             return BaseResult.fail("学生ID不能为空！");
         }
-        if(StringUtils.isEmpty(classId)){
+        if (StringUtils.isEmpty(classId)) {
             return BaseResult.fail("课程ID不能为空！");
         }
         CheckinInfo selectCheckinInfoByKey = checkinInfoService.selectByKey(classId);
@@ -80,12 +105,32 @@ public class StudentController {
         studentCheckin.setCheckinId(checkinInfoId);
         studentCheckin.setStudentId(studentId);
         List<StudentCheckin> selectByExample = studentCheckinService.selectByExample(studentCheckin);
-        if(selectByExample.isEmpty()) {
+        if (selectByExample.isEmpty()) {
             return BaseResult.success("此课程缺席");
-        }
-        else {
+        } else {
             return BaseResult.success("此课程已出席");
         }
     }
 
+    // 用于选课
+    @PostMapping("setClass")
+    public Object setClass(String studentId, String classId) {
+        if (StringUtils.isEmpty(studentId)) {
+            return BaseResult.fail("学生ID不能为空！");
+        }
+        if (StringUtils.isEmpty(classId)) {
+            return BaseResult.fail("课程ID不能为空！");
+        }
+        StudentClass studentClass = new StudentClass();
+        studentClass.setStudentId(studentId);
+        studentClass.setClassId(classId);
+        studentClassService.insert(studentClass);
+        ClassInfo classInfo = classInfoService.selectByKey(classId);
+        return BaseResult.success("选择" + classInfo.getName() + "课程成功");
+    }
+
+    @PostMapping("checkin")
+    public Object checkin(int check, String id) {
+        return null;
+    }
 }
